9c53f59946020c6f50c9f613328a02d1125398b1
Jakelr26 <146326562+Jakelr26@users.noreply.github.com>
2023-11-27 20:28:26 +0000

    Update 09-StringStreams.cpp

--- Tasks/06 - FileIO/09-StringStreams/09-StringStreams.cpp
+++ Tasks/06 - FileIO/09-StringStreams/09-StringStreams.cpp
@@ -16,36 +16,32 @@ int main()
                       "Always. Ok, maybe that's asking too much. " \
                       "How about 3 days-a-week with time off for birthdays?";
 
-    //Write the complete string to the the terminal
-    cout << sentence << endl;
-
     //Read first word (words are separated by spaces or newlines)
     istringstream iss(sentence);
-    iss >> nextWord;
-
+    int coutn = 0;
     //Was a word actually read?
-    if (iss.fail()) {
-        //If it failed, we're probably at the end of the stream
-        cout << "No word successfully read. Is this the end of stream?" << endl;
-    }
-    else {
-        //We have a valid word - so display it
-        cout << "Read the word: " << nextWord << endl;
-
-        //We can compare C++ strings using the == operator (very convenient!)
-        if (nextWord == "May") {
-            cout << "That is what I expected" << endl;
+    
+    while (iss.eof() == false) {
+        
+        if (iss.fail()) {
+            //If it failed, we're probably at the end of the stream
+            cout << "No word successfully read. Is this the end of stream?" << endl;
         }
         else {
-            cout << "Something weird is happening?" << endl;
+            //We have a valid word - so display it
+            cout << nextWord << endl;
+            iss >> nextWord;
+            coutn = coutn + 1;
+        }
+
+        //Final check - did we read an EOF character? This can happen when we read the last word or beyond it (space or newline)
+        if (iss.eof()) {
+            cout << "We reached the end of the file" << endl << endl;
         }
     }
 
-    //Final check - did we read an EOF character? This can happen when we read the last word or beyond it (space or newline)
-    if (iss.eof()) {
-        cout << "We reached the end of the file" << endl;
-    }
-    
+    cout << "final word count is:" << coutn << endl;
+
     //Done
     return 0;
 
